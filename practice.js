8de8668db8906ea5a418bf1e9cb2a41425dcf94b
Wheatley <Wheatley@MacBook-Pro.local>
2017-11-28 08:03:19 -0700

    done save extra

--- practice.js
+++ practice.js
@@ -6,43 +6,35 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+var me = {
+  name: "Tiarra",
+  age: "127"
+}
 
-
-
-
-
+alert(me.name);
 
 //NEXT PROBLEM
 
-
-
-
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
-
-
-
-
-
-
+  var favoriteThings = {
+    band: "Abney Park",
+    food:'sugar', 
+    person: 'Elon Musk',
+    book:"Daemon", 
+    movie: 'LOTR',
+    holiday: 'Halloween'
+  }
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
-
-
-
-
+  favoriteThings.car = "Corvette Stringray";
+  favoriteThings.brand = "none";
 
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 
-  //Code here
-
-
-
-
+  favoriteThings.food = "Chicken Nuggets";
+  favoriteThings.book = "Harry Potter";
 
 //NEXT PROBLEM
 
@@ -58,18 +50,15 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. 
 */
 
-  //Code here
+  var backPack = {};
+  var item = "firstPocket";
 
-
-
-
-
+  backPack[item] = 'chapstick';
+  backPack.color = 'brown';
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
-
- 
+  alert(backPack);
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -77,11 +66,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-  //Code here
-
-
-
-
+  console.log(backPack);
 
 //NEXT PROBLEM
 
@@ -99,7 +84,8 @@ var user2 = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+  user2.name = 'Tyler';
+  user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
